be20e3ce86a3680e2fbfbd24b6dbd03f4d6e4721
Roman <iamromandev@gmail.com>
2023-10-03 04:52:27 +0600

    bot+cli - first arg print

--- cli/cli/__main__.py
+++ cli/cli/__main__.py
@@ -6,6 +6,8 @@ from .funcmodule import my_function
 def main():
     print('in main')
     args = sys.argv[1:]
+    func = sys.argv[0]
+    print("First Arg {}".format(func))
     print('count of args :: {}'.format(len(args)))
     for arg in args:
         print('passed argument :: {}'.format(arg))
